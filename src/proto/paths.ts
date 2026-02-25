481987156129d3e8c0aa59e48b5b44699685366b
Maksim <sharkomaks@gmail.com>
2026-02-25 18:24:47 +0300

    feat: add users.proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -2,5 +2,6 @@ import { join } from 'node:path';
 
 export const PROTO_PATHS = {
 	AUTH: join(__dirname, '../../proto/auth.proto'),
-	ACCOUNT: join(__dirname, '../../proto/account.proto')
+	ACCOUNT: join(__dirname, '../../proto/account.proto'),
+	USERS: join(__dirname, '../../proto/users.proto'),
 } as const;
